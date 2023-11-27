381b0cd9d3904c180e79daee88bd92007ad79c4d
ShivaKumarReddyB <b.skreddy1997@gmail.com>
2023-11-27 11:12:57 -0600

    small fix

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,15 +3,6 @@ var router = express.Router();
 
 var express = require("express");
 const vehicleControllers = require("../controllers/vehicleController");
-// var router = express.Router();
-/* GET costumes */
-router.get("/", vehicleControllers.vehicle_view_all_Page);
-
-/* GET detail Vehicle page */
-router.get("/detail", vehicleControllers.vehicle_view_one_Page);
-/* GET create vehicle page */
-router.get("/create", secured, vehicleControllers.vehicle_create_Page);
-
 // A little function to check if we have an authorized user and continue on
 //or;
 // redirect to login.
@@ -21,6 +12,16 @@ const secured = (req, res, next) => {
   }
   res.redirect("/login");
 };
+
+// var router = express.Router();
+/* GET costumes */
+router.get("/", vehicleControllers.vehicle_view_all_Page);
+
+/* GET detail Vehicle page */
+router.get("/detail", vehicleControllers.vehicle_view_one_Page);
+/* GET create vehicle page */
+router.get("/create", secured, vehicleControllers.vehicle_create_Page);
+
 /* GET create update page */
 router.get("/update", secured, vehicleControllers.vehicle_update_Page);
 // /* GET delete vehicle page */
